b93e4cfb4c59a2d84ebf2e08c6bdc431d7088d14
David-AGDLC <24000588@galileo.edu>
2024-08-05 20:03:52 -0600

    version final

--- backend/index.js
+++ backend/index.js
@@ -21,9 +21,42 @@ app.get("/", (req, res) => {
   .then(Usuarios=>res.json(Usuarios))
   .catch(err=>res.json(err))
 });
+app.get("/usuario/:id", (req, res) => {
+  const id = req.params.id;
+  paquete1.findById({_id:id})
+  .then(Usuarios=>res.json(Usuarios))
+  .catch(err=>res.json(err))
+});
+app.put("/actualizar/:id", (req,res)=>{
+  const id = req.params.id;
+  paquete1.findByIdAndUpdate({_id:id}, {
+    nombre: req.body.nombre,
+    Email: req.body.Email,
+    numero: req.body.numero,
+    departamento: req.body.departamento})
+  .then(Usuarios=>res.json(Usuarios))
+  .catch(err=>res.json(err))
+})
+app.delete("/eliminar/:id",(req,res)=>{
+  const id=req.params.id
+  paquete1.findByIdAndDelete({_id:id})
+  .then(res =>res.json(res))
+  .catch(err =>res.json(err))
+})
 app.post("/anadir", (req, res) => {
   paquete1.create(req.body)
   .then(Usuarios=>res.json(Usuarios))
   .catch(err=>res.json(err))
 });
-
+app.get("/contar-usuarios", (req, res) => {
+  paquete1.aggregate([
+    {
+      $group: {
+        _id: "$departamento",
+        count: { $sum: 1 }
+      }
+    }
+  ])
+    .then(result => res.json(result))
+    .catch(err => res.json(err));
+});
